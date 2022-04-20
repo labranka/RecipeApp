d62eed3c3471deef8c4b5c2975c839e8dc7399ce
labranka <brankarbc96@gmail.com>
2022-04-20 23:02:24 +0200

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -122,6 +122,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the application');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -130,5 +134,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 init();
