c9bea70f4600e67172d1afbb4eaea3e5c42f4bb7
labranka <brankarbc96@gmail.com>
2022-04-20 20:49:06 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'cad89b20-1c9f-4182-a49c-b02c38eceb7b';
+export const MODAL_CLOSE_SEC = 2.5;
