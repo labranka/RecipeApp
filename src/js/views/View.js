148f5b09d2ae61e96c7f9e2cad8d65c172fed799
labranka <brankarbc96@gmail.com>
2022-04-21 03:07:18 +0200

    Izmena

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -4,10 +4,10 @@ export default class View {
   _data;
 
   /**
-   * Render the received object to DOM
+   * Render the received object to the DOM
    * @param {Object | Object[]} data The data to be rendered (e.g. recipe)
-   * @param {boolean} [render = true] If false, create markup string instead of rendering to the DOM
-   * @returns {undefined | string} A markup string is returned if render = false
+   * @param {boolean} [render=true] If false, create markup string instead of rendering to the DOM
+   * @returns {undefined | string} A markup string is returned if render=false
    * @this {Object} View instance
    * @author Branka Lakicevic
    * @todo Finish implementation
@@ -35,21 +35,22 @@ export default class View {
 
     newElements.forEach((newEl, i) => {
       const curEl = curElements[i];
+      // console.log(curEl, newEl.isEqualNode(curEl));
 
-      //Updates chaged TEXT
+      // Updates changed TEXT
       if (
         !newEl.isEqualNode(curEl) &&
         newEl.firstChild?.nodeValue.trim() !== ''
       ) {
+        // console.log('💥', newEl.firstChild.nodeValue.trim());
         curEl.textContent = newEl.textContent;
       }
 
-      //Updates changed ATTRIBUTES
-      if (!newEl.isEqualNode(curEl)) {
+      // Updates changed ATTRIBUES
+      if (!newEl.isEqualNode(curEl))
         Array.from(newEl.attributes).forEach(attr =>
           curEl.setAttribute(attr.name, attr.value)
         );
-      }
     });
   }
 
@@ -60,41 +61,41 @@ export default class View {
   renderSpinner() {
     const markup = `
       <div class="spinner">
-      <svg>
-        <use href="${icons}#icon-loader"></use>
-      </svg>
-    </div> `;
-
+        <svg>
+          <use href="${icons}#icon-loader"></use>
+        </svg>
+      </div>
+    `;
     this._clear();
     this._parentElement.insertAdjacentHTML('afterbegin', markup);
   }
 
   renderError(message = this._errorMessage) {
     const markup = `
-              <div class="error">
-                <div>
-                  <svg>
-                    <use href="${icons}#icon-alert-triangle"></use>
-                  </svg>
-                </div>
-                <p>${message}</p>
-              </div>
-      `;
+      <div class="error">
+        <div>
+          <svg>
+            <use href="${icons}#icon-alert-triangle"></use>
+          </svg>
+        </div>
+        <p>${message}</p>
+      </div>
+    `;
     this._clear();
     this._parentElement.insertAdjacentHTML('afterbegin', markup);
   }
 
   renderMessage(message = this._message) {
     const markup = `
-              <div class="message">
-                <div>
-                  <svg>
-                  <use href="${icons}#icon-smile"></use>
-                  </svg>
-                </div>
-                <p>${message}</p>
-              </div>
-      `;
+      <div class="message">
+        <div>
+          <svg>
+            <use href="${icons}#icon-smile"></use>
+          </svg>
+        </div>
+        <p>${message}</p>
+      </div>
+    `;
     this._clear();
     this._parentElement.insertAdjacentHTML('afterbegin', markup);
   }
